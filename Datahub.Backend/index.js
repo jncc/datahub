2d712c9f25ed6a702203a453c8cfd07bdb983159
Pete Montgomery <sumo.the.cat@gmail.com>
2018-10-26 12:09:32 +0100

    use IAM authentication

--- Datahub.Backend/index.js
+++ Datahub.Backend/index.js
@@ -1,27 +1,9 @@
 
-// const AWS = require('aws-sdk');
-
-// let dynamo = new AWS.DynamoDB.DocumentClient();
-
-// exports.upsert = async function (event, context) {
-
-// 	// log to cloudwatch
-// 	console.log(event);
-
-// 	upsertIntoDynamo(event);
-
-// 	context.succeed('Received ' + event.id);
-// };
-
-// let upsertIntoDynamo = function(event) {
-// 	// todo
-// }
-
 const AWS = require('aws-sdk')
 const ClaudiaApiBuilder = require('claudia-api-builder')
 
-var api = new ClaudiaApiBuilder()
-var dynamo = new AWS.DynamoDB.DocumentClient()
+const api = new ClaudiaApiBuilder()
+const dynamo = new AWS.DynamoDB.DocumentClient()
 
 api.get('/hello', () => 'Hello!')
 
@@ -37,22 +19,22 @@ api.post('/assets',
           id: req.body.asset.id,
           // other properties go here
       } 
-		};
+		}
 
 		// put into the database
-    return dynamo.put(params).promise();
+    return dynamo.put(params).promise()
   },
-  { success: 201 }
+  {
+    success: 201,
+    authorizationType: 'AWS_IAM',
+  }
 )
 
 api.get('/assets',
   async function (req) {
-    // return dynamo.scan({ TableName: 'datahub-assets' })
-    //   .promise()
-    //   .then(response => response.Items)
 		let response = await dynamo.scan({ TableName: 'datahub-assets' }).promise()
 		return response.Items
   }
-);
+)
 
 module.exports = api
