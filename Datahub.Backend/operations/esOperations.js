384ebebf7719e7073ef6d4dcdaab73a6b6f50229
Pete Montgomery <sumo.the.cat@gmail.com>
2018-10-31 16:48:20 +0000

    extract makeElasticSearchRequest

--- Datahub.Backend/operations/esOperations.js
+++ Datahub.Backend/operations/esOperations.js
@@ -21,56 +21,69 @@ module.exports.esauthtest = async function(req) {
 
   console.log('Hello from esauthtest')
 
-  var region = 'eu-west-1'; // e.g. us-west-1
-  var domain = process.env.ES_DOMAIN;
-  var index = 'node-test';
-  var type = '_doc';
-  var id = '1';
-  var document = {
-    "title": "Moneyball",
-    "director": "Bennett Miller",
-    "year": "2011"
-  }
-  
-  console.log('Domain is ' + domain)
-  
-  var endpoint = new AWS.Endpoint(domain);
-  var request = new AWS.HttpRequest(endpoint, region);
-
-  request.method = 'PUT';
-  request.path += index + '/' + type + '/' + id;
-  request.body = JSON.stringify(document);
-  request.headers['host'] = domain;
-  request.headers['Content-Type'] = 'application/json';
-
-  var credentials = new AWS.EnvironmentCredentials('AWS');
-  var signer = new AWS.Signers.V4(request, 'es');
-  signer.addAuthorization(credentials, new Date());
-
-  var client = new AWS.HttpClient();
-  console.log('Sending request ')
-
-  await new Promise((resolve, reject) => {
-
-    client.handleRequest(request, null,
-      function(response) {
-        console.log('Success!!! ' + response.statusCode + ' ' + response.statusMessage);
-        var r = '';
-        response.on('data', function (chunk) {
-          r += chunk;
-        });
-        response.on('end', function (chunk) {
-          console.log('Response body: ' + r);
-          resolve(r)
-        });
-      },
-      function(error) {
-        console.log('Error!!! ' + error);
-        reject(error)
-      }
-    );
-  })    
-
+  await makeElasticSearchRequest({
+    method: 'POST',
+    path: 'node-test/_doc/2',
+    body: {
+     "title": "Moneyball",
+     "director": "Bennett Miller",
+     "year": "2011"
+   }
+ })
 
   return 'Done.'
+}
+
+/**
+ * Makes an AWS ElasticSearch Service (ES) request.
+ */
+let makeElasticSearchRequest = ({method, path, body}) => {
+
+  // this function is essentially taken from the AWS example here:
+  // https://docs.aws.amazon.com/elasticsearch-service/latest/developerguide/es-request-signing.html#es-request-signing-node
+
+  // var region = 'eu-west-1'; // e.g. us-west-1
+  // var domain = process.env.ES_DOMAIN;
+  // var index = 'node-test';
+  // var type = '_doc';
+  // var id = '1';
+  // var document = {
+  //   "title": "Moneyball",
+  //   "director": "Bennett Miller",
+  //   "year": "2011"
+  // }
+  // console.log('Domain is ' + domain)
+
+
+  let endpoint = new AWS.Endpoint(domain)
+  let r = new AWS.HttpRequest(endpoint, 'eu-west-1') // todo: process.env.AWS_REGION
+
+  r.method = method
+  r.path += path // index + '/' + type + '/' + id; ... todo: why is this '+='?
+  r.headers['host'] = process.env.ES_DOMAIN
+  r.headers['Content-Type'] = 'application/json'
+  r.body = JSON.stringify(body)
+
+  // sign the request with the current IAM principal
+  let credentials = new AWS.EnvironmentCredentials('AWS')
+  let signer = new AWS.Signers.V4(r, 'es')
+  signer.addAuthorization(credentials, new Date())
+
+  let httpClient = new AWS.HttpClient()
+
+  // return a promise so we can await it, rather than letting the aws lambda just finish!
+  return new Promise((resolve, reject) => {
+    httpClient.handleRequest(r, null,
+      (response) => {
+        // console.log('Success!!! ' + response.statusCode + ' ' + response.statusMessage)
+        var s = ''
+        response.on('data', (chunk) => { s += chunk })
+        response.on('end', () => { resolve(s) })
+      },
+      (error) => {
+        // console.log('Error!!! ' + error)
+        reject(error)
+      }
+    )
+  })
 }
