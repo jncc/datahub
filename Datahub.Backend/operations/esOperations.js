3d5126dbc04dcbe87ce331111eb0e45c6399d358
Pete Montgomery <sumo.the.cat@gmail.com>
2018-12-13 22:27:16 +0000

    validate published_date

--- Datahub.Backend/operations/esOperations.js
+++ Datahub.Backend/operations/esOperations.js
@@ -27,7 +27,7 @@ module.exports.putDocument = async function(req) {
 
   await sendSignedRequest({
     method: 'PUT',
-    path: 'main/_doc/' + body.id,
+    path: 'main/_doc/' + req.body.id,
     body: req.body,
   })
 
@@ -39,7 +39,7 @@ function validateDocument(doc) {
   if (!doc.id) {
     throw 'doc.id is required.'
   }
-  if (!/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(doc.id)) {
+  if (! /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(doc.id)) {
     throw 'doc.id must be a regex' // this is a first-stab to ensure that IDs are unique
   }
   if (!doc.site) {
@@ -53,6 +53,13 @@ function validateDocument(doc) {
   }
   if (!doc.content) {
     throw 'doc.content is required.'
+  }
+  if (!doc.published_date) {
+    throw 'doc.published_date is required.'
+  }
+  // https://www.myintervals.com/blog/2009/05/20/iso-8601-date-validation-that-doesnt-suck/
+  if (! /^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/.test()) {
+    throw 'doc.published_date must be an ISO 8601 date. (E.g., \'2018\' will do!)'
   }
 
   // todo: more validation
