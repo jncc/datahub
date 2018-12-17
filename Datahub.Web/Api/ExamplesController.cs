ffa174867121588367f66ab637a54f94bb4eee50
Matt Debont <matt.debont@jncc.gov.uk>
2018-12-17 11:26:54 +0000

    Basic working get example with config and highlighting

--- Datahub.Web/Api/ExamplesController.cs
+++ Datahub.Web/Api/ExamplesController.cs
@@ -7,28 +7,26 @@ using System.Threading.Tasks;
 using Aws4RequestSigner;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Datahub.Web.Models;
+using Datahub.Web.Elasticsearch;
+using Nest;
 
 public class ExamplesController : Controller
-{ 
-    [HttpGet("/api/examples/esget")]
-    public async Task<IActionResult> ESGet(int id)
+{
+    private readonly ElasticClient _client;
+    private readonly IElasticsearchService _elasticsearchService;
+
+    public ExamplesController(IElasticsearchService elasticsearchService)
     {
-        string query = @"
-            {
-                ""query"" : {
-                    ""simple_query_string"": { ""query"": ""example"" }
-                }
-            }
-            ".Trim();
+        _client = elasticsearchService.Client();
+        _elasticsearchService = elasticsearchService;
+    }
 
-        var request = new HttpRequestMessage
-        {
-            Method = HttpMethod.Get,
-            RequestUri = new Uri(GetESEndpointUrl() + "main/_search"),
-            Content = new StringContent(query, Encoding.UTF8, "application/json")
-        };
 
-        var signedRequest = await GetSignedRequest(request);
+    [HttpGet("/api/examples/esget")]
+    public async Task<IActionResult> ESGet(string q = null, List<Keyword> k = null, string site = "datahub", int start = 0, int size = 10)
+    {
+        var signedRequest = await GetSignedRequest(BuildSearchQuery(q, k, site, start, size));
         var response = await new HttpClient().SendAsync(signedRequest);
         var responseString = await response.Content.ReadAsStringAsync();        
 
@@ -41,16 +39,51 @@ public class ExamplesController : Controller
         return await signer.Sign(request, "es", Env.Var.ESAwsRegion);
     }
 
+
+    HttpRequestMessage BuildSearchQuery(string query = null, List<Keyword> keywords = null, string site = null, int start = 0, int size = 10)
+    {
+        string q = string.Format(@"{{
+            ""_source"": {{ ""excludes"": [ ""content"" ] }},
+            ""from"": {0},
+            ""size"": {1},
+            ""query"": {{
+                ""bool"": {{
+                    ""filter"": [
+                        {{ ""term"": {{ ""site"": ""{2}"" }} }}
+                    ],
+                    ""must"": [
+                        {{ ""common"": {{ ""content"": {{ ""query"": ""{3}"", ""cutoff_frequency"": 0.001, ""low_freq_operator"": ""or"" }} }} }}
+                    ]
+                }}
+            }},
+            ""highlight"": {{
+                ""fields"": {{ ""content"": {{}} }}
+            }}
+        }}", start, size, site, query).Trim();
+
+        UriBuilder uriBuilder = new UriBuilder(GetESEndpointUrl());
+        uriBuilder.Path = "main/_search";
+
+
+        return new HttpRequestMessage
+        {
+            Method = HttpMethod.Get,
+            RequestUri = new Uri(GetESEndpointUrl() + "main/_search"),
+            Content = new StringContent(q, Encoding.UTF8, "application/json")
+        };
+        
+    }
+
     string GetESEndpointUrl()
     {
         // the reason for building the endpoint URL from host, scheme and port here is obscure
         // and apparently helps to solve a problem when reading environment variables on elastic beanstalk
         // (there's no need to make it this complicated outside elasticbeanstalk!)
         var uri = new UriBuilder() { Host = Env.Var.ESEndpointHost };
-        if (!String.IsNullOrEmpty(Env.Var.ESEndpointScheme)) 
+        if (!string.IsNullOrEmpty(Env.Var.ESEndpointScheme)) 
             uri.Scheme = Env.Var.ESEndpointScheme;
-        if (!String.IsNullOrEmpty(Env.Var.ESEndpointPort))
-            uri.Port = Int32.Parse(Env.Var.ESEndpointPort);
+        if (!string.IsNullOrEmpty(Env.Var.ESEndpointPort))
+            uri.Port = int.Parse(Env.Var.ESEndpointPort);
 
         return uri.ToString();
     }
@@ -78,11 +111,15 @@ public class Env
         ESAwsSecretAccessKey = GetVariable("ELASTICSEARCH_AWS_SECRETACCESSKEY");
         ESEndpointHost = GetVariable("ELASTICSEARCH_HOST");
         ESEndpointScheme = GetVariable("ELASTICSEARCH_HOST_SCHEME");
-        ESEndpointPort = GetVariable("ELASTICSEARCH_HOST_PORT");
+        ESEndpointPort = GetVariable("ELASTICSEARCH_HOST_PORT", true);
     }
 
-    string GetVariable(string variable)
+    string GetVariable(string variable, bool optional = false)
     {
+        if (optional)
+        {
+            return null;
+        }
         return Environment.GetEnvironmentVariable(variable)
             ?? throw new Exception($"The environment variable {variable} couldn't be read. You may need to define it in your .env file.");
     }
---
+++ Datahub.Web/Models/Query.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Datahub.Web.Models
+{
+    public class Query
+    {
+        [JsonProperty(PropertyName = "query")]
+        string test;
+    }
+}
