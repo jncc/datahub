09e8bf2bb091e10d8b7d828b55676f25ba1990aa
Pete Montgomery <sumo.the.cat@gmail.com>
2019-02-20 15:06:05 +0000

    improve Search page slightly #73

--- Datahub.Web/Pages/Search.cshtml.cs
+++ Datahub.Web/Pages/Search.cshtml.cs
@@ -12,54 +12,53 @@ namespace Datahub.Web.Pages
 {
     public class SearchModel : PageModel
     {
-        public const string DefaultIndex = "test";
-        public const string DefaultSite = "datahub";
-        public const int DefaultSize = 10;
-        public const int DefaultStart = 0;
+        const string ES_INDEX = "test";
+        const string ES_SITE = "datahub";
+        
+        const int DefaultSize = 10;
+        const int DefaultStart = 0;
 
         private readonly IHostingEnvironment _env;
-        private readonly ElasticClient _client;
+        private readonly IElasticsearchService _esService;
 
         public ISearchResponse<SearchResult> Results { get; set; }
 
-        [BindProperty(Name = "q", SupportsGet = true)]
-        public string QueryString{ get; set; }
-        [BindProperty(Name = "p", SupportsGet = true)]
-        public int CurrentPage { get; set; }
-        [BindProperty(Name = "s", SupportsGet = true)]
-        public int CurrentPageSize { get; set; }
+        public string QueryString      { get; private set; }
+        public int    CurrentPage      { get; private set; }
+        public int    CurrentPageSize  { get; private set; }
 
         public List<Keyword> Keywords { get; set; }
 
         public SearchModel(IHostingEnvironment env, IElasticsearchService elasticsearchService)
         {
             _env = env;
-            _client = elasticsearchService.Client();
+            _esService = elasticsearchService;
         }
         
         public async Task OnGetAsync(string q, string[] k, int p = 1, int size = DefaultSize)
         {
             CurrentPageSize = size;
             CurrentPage = p;
+
             Keywords = ParseKeywords(k);
 
-                Results = await _client.SearchAsync<SearchResult>(s => s
-                    .Index(DefaultIndex)
-                    .From(ElasticsearchService.GetStartFromPage(p, size))
-                    .Size(size)
-                    .Source(src => src
-                        .IncludeAll()
-                        .Excludes(e => e
-                            .Field(f => f.Content)
-                        )
+            Results = await _esService.Client().SearchAsync<SearchResult>(s => s
+                .Index(ES_INDEX)
+                .From(ElasticsearchService.GetStartFromPage(p, size)) // todo check this
+                .Size(size)
+                .Source(src => src
+                    .IncludeAll()
+                    .Excludes(e => e
+                        .Field(f => f.Content)
                     )
-                    .Query(l => ElasticsearchService.BuildDatahubQuery(q, ParseKeywords(k), DefaultSite))
-                    .Highlight(h => h
-                        .Fields(f => f.Field(x => x.Content))
-                        .PreTags("<b>")
-                        .PostTags("</b>")
-                    )
-                );
+                )
+                .Query(l => ElasticsearchService.BuildDatahubQuery(q, ParseKeywords(k), ES_SITE))
+                .Highlight(h => h
+                    .Fields(f => f.Field(x => x.Content))
+                    .PreTags("<b>")
+                    .PostTags("</b>")
+                )
+            );
         }
 
         private List<Keyword> ParseKeywords(string[] keywords)
