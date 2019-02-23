dcba02774a6798f1baa4a191ab1966156ef963ff
Pete Montgomery <sumo.the.cat@gmail.com>
2019-02-23 12:55:31 +0000

    search params can have their own type

--- Datahub.Web/Pages/Search.cshtml.cs
+++ Datahub.Web/Pages/Search.cshtml.cs
@@ -13,47 +13,29 @@ namespace Datahub.Web.Pages
 {
     public class SearchModel : PageModel
     {
-        const string ES_SITE = "datahub";
-        
-        const int DefaultPage = 0;
-        const int DefaultPageSize = 10;
+        readonly IEnv env;
+        readonly ISearchBuilder searchBuilder;
+        readonly IElasticsearchService esService;
 
-        readonly IEnv _env;
-        readonly ISearchBuilder _searchBuilder;
-        readonly IElasticsearchService _esService;
-
-        public ISearchResponse<SearchResult> Results { get; set; }
-
-        public string QueryString      { get; private set; }
-        public int    CurrentPage      { get; private set; }
-        public int    CurrentPageSize  { get; private set; }
-        public string ESQueryJson      { get; private set; }
-
+        // view model properties
+        public SearchParams SearchParams { get; private set; }
         public List<Keyword> Keywords { get; set; }
+        public ISearchResponse<SearchResult> Results { get; set; }
 
         public SearchModel(IEnv env, ISearchBuilder searchBuilder, IElasticsearchService esService)
         {
-            _env = env;
-            _searchBuilder = searchBuilder;
-            _esService = esService;
+            this.env = env;
+            this.searchBuilder = searchBuilder;
+            this.esService = esService;
         }
         
         public async Task OnGetAsync(SearchParams input)
         {
-            System.Console.WriteLine("Hello");
-            System.Console.WriteLine(input.q);
+            SearchParams = input;
+            Keywords = searchBuilder.ParseKeywords(input.k);
 
-            CurrentPageSize = input.size; //todo rename 'size'
-            CurrentPage = input.p;
-            Keywords = _searchBuilder.ParseKeywords(input.k);
-
-            var search = _searchBuilder.BuildQuery(input);
-
-            var client = _esService.Client();
-
-            var json = client.RequestResponseSerializer.SerializeToString(search);
-            System.Console.WriteLine(json);
-
+            var search = searchBuilder.BuildQuery(input);
+            var client = esService.Client();
             Results = await client.SearchAsync<SearchResult>(search);
         }
     }
