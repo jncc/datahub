efacf4d23a871525aa1a73f93f536583bbfd4265
felix mason <felix.mason@jncc.gov.uk>
2019-04-15 18:11:43 +0100

    tidy keyword add

--- Datahub.Web/wwwroot/js/site.js
+++ Datahub.Web/wwwroot/js/site.js
@@ -1,8 +1,33 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
-function removeKeyword(vocab, value, buttonId) {
-    x = location.search;
-    alert(x);
-    k=vocab + value;
-    $('#' + buttonId).hide();
+function removeKeyword(index) {
+    $('#keyword' + index).remove();
+    $('#removeKeyword' + index).remove();
+    $('form').submit();
+}
+
+function isFilteredKeyword(k) {
+    var exists = false;
+
+    $('input[name=k]').each((item, element) => {
+        if (element.value === k) {
+            exists = true;
+        }
+    });
+
+    return exists;
+}
+
+function addKeyword(vocab, value) {
+    k = vocab + '/' + value;
+    
+    if (! isFilteredKeyword(k)) {
+        $('<input>').attr({
+            type: 'hidden',
+            name: 'k',
+            value: k
+        }).appendTo('#keywords');
+
+        $('form').submit();
+    } 
 }
