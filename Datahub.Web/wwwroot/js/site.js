f7cd3ab58a8436df644c85cc4e4354c3abcf17c4
felix mason <felix.mason@jncc.gov.uk>
2019-05-09 14:55:20 +0100

    keyword search

--- Datahub.Web/wwwroot/js/site.js
+++ Datahub.Web/wwwroot/js/site.js
@@ -1,4 +1,34 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your Javascript code.
+function removeKeyword(index) {
+    $('#keyword' + index).remove();
+    $('#removeKeyword' + index).remove();
+    $('form').submit();
+}
+
+function isFilteredKeyword(k) {
+    var exists = false;
+
+     $('input[name=k]').each((item, element) => {
+        if (element.value === k) {
+            exists = true;
+        }
+    });
+
+     return exists;
+}
+
+function addKeyword(vocab, value) {
+    k = vocab + '/' + value;
+
+     if (! isFilteredKeyword(k)) {
+        $('<input>').attr({
+            type: 'hidden',
+            name: 'k',
+            value: k
+        }).appendTo('#keywords');
+
+         $('form').submit();
+    } 
+}
