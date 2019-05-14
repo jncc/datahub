98f8b7705a1dad9273a49e853f59c594ce32de5c
Pete Montgomery <sumo.the.cat@gmail.com>
2019-05-14 13:23:33 +0100

    merge work on search pager and extra keyword css

--- Datahub.Web/Pages/Search.cshtml.cs
+++ Datahub.Web/Pages/Search.cshtml.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -19,6 +20,7 @@ namespace Datahub.Web.Pages
         public SearchParams SearchParams { get; private set; }
         public List<Keyword> Keywords { get; private set; }
         public ISearchResponse<SearchResult> Results { get; private set; }
+        public ResultsPager Pager { get; private set; }
 
         public SearchModel(ISearchBuilder searchBuilder, IElasticsearchService esService)
         {
@@ -28,12 +30,104 @@ namespace Datahub.Web.Pages
         
         public async Task OnGetAsync(SearchParams input)
         {
-            SearchParams = input;
-            Keywords = searchBuilder.ParseKeywords(input.k);
-
             var search = searchBuilder.BuildQuery(input);
             var client = esService.Client();
-            Results = await client.SearchAsync<SearchResult>(search);
+            var results = await client.SearchAsync<SearchResult>(search);
+            
+            Console.WriteLine(input.p);
+
+            Results = results;
+            SearchParams = input;
+            Keywords = searchBuilder.ParseKeywords(input.k);
+            Pager = new ResultsPager(
+                totalItems:  (int) results.Total,
+                currentPage: input.p,
+                pageSize:    input.size,
+                maxPages:    5);
         }
     }
+
+
+    public class ResultsPager
+    {
+        public ResultsPager(
+            int totalItems,
+            int currentPage = 1,
+            int pageSize = 10,
+            int maxPages = 10)
+        {
+            // calculate total pages
+            var totalPages = (int)Math.Ceiling((decimal)totalItems / (decimal)pageSize);
+
+            // ensure current page isn't out of range
+            if (currentPage < 1)
+            {
+                currentPage = 1;
+            }
+            else if (currentPage > totalPages)
+            {
+                currentPage = totalPages;
+            }
+
+            int startPage, endPage;
+            if (totalPages <= maxPages) 
+            {
+                // total pages less than max so show all pages
+                startPage = 1;
+                endPage = totalPages;
+            }
+            else 
+            {
+                // total pages more than max so calculate start and end pages
+                var maxPagesBeforeCurrentPage = (int)Math.Floor((decimal)maxPages / (decimal)2);
+                var maxPagesAfterCurrentPage = (int)Math.Ceiling((decimal)maxPages / (decimal)2) - 1;
+                if (currentPage <= maxPagesBeforeCurrentPage) 
+                {
+                    // current page near the start
+                    startPage = 1;
+                    endPage = maxPages;
+                } 
+                else if (currentPage + maxPagesAfterCurrentPage >= totalPages) 
+                {
+                    // current page near the end
+                    startPage = totalPages - maxPages + 1;
+                    endPage = totalPages;
+                }
+                else 
+                {
+                    // current page somewhere in the middle
+                    startPage = currentPage - maxPagesBeforeCurrentPage;
+                    endPage = currentPage + maxPagesAfterCurrentPage;
+                }
+            }
+
+            // calculate start and end item indexes
+            var startIndex = (currentPage - 1) * pageSize;
+            var endIndex = Math.Min(startIndex + pageSize - 1, totalItems - 1);
+
+            // create an array of pages that can be looped over
+            var pages = Enumerable.Range(startPage, (endPage + 1) - startPage);
+
+            // update object instance with all pager properties required by the view
+            TotalItems = totalItems;
+            CurrentPage = currentPage;
+            PageSize = pageSize;
+            TotalPages = totalPages;
+            StartPage = startPage;
+            EndPage = endPage;
+            StartIndex = startIndex;
+            EndIndex = endIndex;
+            Pages = pages;
+        }
+
+        public int TotalItems { get; private set; }
+        public int CurrentPage { get; private set; }
+        public int PageSize { get; private set; }
+        public int TotalPages { get; private set; }
+        public int StartPage { get; private set; }
+        public int EndPage { get; private set; }
+        public int StartIndex { get; private set; }
+        public int EndIndex { get; private set; }
+        public IEnumerable<int> Pages { get; private set; }
+    }    
 }
