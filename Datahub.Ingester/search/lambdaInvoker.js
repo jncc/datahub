3c0a9c2226cd2416dc3acdacc4f5ca841adff8b4
CathyJinJNCC <cathy.jin@jncc.gov.uk>
2021-11-10 10:18:25 +0000

    Delete using lambda

---
+++ Datahub.Ingester/search/lambdaInvoker.js
@@ -0,0 +1,42 @@
+const env = require('../env')
+const AWS = require('aws-sdk')
+
+const sts = new AWS.STS()
+
+module.exports.deleteByAssetId = async function (id, index) {
+  var errors = []
+
+  // Assume role from jncc-website-x to do the invoke
+  var stsParams = {
+    RoleArn: '',
+    RoleSessionName: ''
+  }
+
+  console.error(`Assuming role for lambda invocation`)
+  var assumedRole = sts.assumeRole(stsParams).promise()
+  
+  var tempCredentials = {
+    accessKeyId: assumedRole.Credentials.AccessKeyId,
+    secretAccessKey: assumedRole.Credentials.SecretAccessKey,
+    sessionToken: assumedRole.Credentials.SessionToken,
+  }
+
+  var lambda = new AWS.Lambda(tempCredentials)
+
+  var lambdaParams = {
+    FunctionName: env.SEARCH_DELETER_LAMBDA,
+    InvocationType: 'RequestResponse',
+    LogType: 'Tail',
+    Payload: `{ "id": "${id}", "index": "${index}" }`
+  }
+
+  console.error(`Invoking lambda ${env.SEARCH_DELETER_LAMBDA}`)
+  lambda.invoke(lambdaParams, function(err, data) {
+    if (err) {
+      console.error(`${env.SEARCH_DELETER_LAMBDA} lambda delete by asset ID '${id}' request failed: ${err}`)
+      errors.push(err)
+    }
+  })
+
+  return { success: errors.length === 0, messages: errors }
+}
