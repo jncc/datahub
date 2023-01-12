942e8c6658ee0af4e3906dfecc218faf73a98ef1
martin bamford <martin.bamford@jncc.gov.uk>
2023-01-12 09:29:28 +0000

    Initial python rewrite that works from command line with provided parameters

---
+++ Datahub.Sitemap2.0/Parameters.py
@@ -0,0 +1,9 @@
+parameters = {
+    "table": "datahub-beta-assets",
+    "scheme": "https",
+    "host": "hub.jncc.gov.uk",
+    "base_path": "assets",
+    "change_frequency": "monthly",
+    "bucket": "jncc-sitemaps",
+    "key": "sitemap.xml"
+}
