09fa4ba2098e492b58a065f6572aa3e090173e03
martin bamford <martin.bamford@jncc.gov.uk>
2023-01-13 09:21:42 +0000

    Revert "Initial python rewrite that works from command line with provided parameters"
    This reverts commit 942e8c6658ee0af4e3906dfecc218faf73a98ef1.

--- Datahub.Sitemap2.0/Parameters.py
+++
@@ -1,9 +0,0 @@
-parameters = {
-    "table": "datahub-beta-assets",
-    "scheme": "https",
-    "host": "hub.jncc.gov.uk",
-    "base_path": "assets",
-    "change_frequency": "monthly",
-    "bucket": "jncc-sitemaps",
-    "key": "sitemap.xml"
-}
