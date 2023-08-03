27bde9de10354850d3052592b9df4056c883606b
CathyJinJNCC <cathy.jin@jncc.gov.uk>
2023-08-03 13:55:06 +0100

    Fixing s3Key casing

--- Datahub.Ingester/search/s3MessageUploader.js
+++ Datahub.Ingester/search/s3MessageUploader.js
@@ -8,20 +8,21 @@ const s3 = new AWS.S3({
 })
 
 module.exports.uploadMessageToS3 = async function (message, config) {
-  console.log(`S3 - Saving large message for doc ${message.document.id} with title ${message.document.title} and asset ID ${message.document.asset_id}`)
-
   var errors = []
   var s3Key = uuid4()
+  var bucket = config.sqs.largeMessageBucket
   var params = {
-    Bucket: config.sqs.largeMessageBucket,
+    Bucket: bucket,
     Key: s3Key,
     Body: JSON.stringify(message)
   }
+
+  console.log(`S3 - Saving large message for doc ${message.document.id} with title ${message.document.title} and asset ID ${message.document.asset_id} at ${s3Key} in bucket ${bucket}`)
 
   await s3.putObject(params).promise().catch((error) => {
     console.error(`Failed to put message ${message.document.id} into S3 with key ${s3Key}, error: ${error}`)
     errors.push(error)
   })
 
-  return { success: errors.length === 0, errors: errors, s3key: s3Key }
+  return { success: errors.length === 0, errors: errors, s3Key: s3Key }
 }
